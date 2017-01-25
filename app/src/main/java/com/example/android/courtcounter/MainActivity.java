21765bc574ee1e33074cb5d47deb6fe50e399147
aviralsharma021 <aviralsharma021@gmail.com>
2017-01-26 01:57:28 +0530

    creation_of_reset_button

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -15,33 +15,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /** To increase the score of the Team by 2**/
     public void Aincrement2(View view){
         scoreOfA+=2;
         displayforTeamA(scoreOfA);
     }
 
+    /** To increase the score of the Team by 3**/
     public void Aincrement3(View view){
         scoreOfA+=3;
         displayforTeamA(scoreOfA);
     }
 
+    /** To increase the score of the Team by 1**/
     public void Afreethrow(View view){
         scoreOfA++;
         displayforTeamA(scoreOfA);
     }
 
+    /** To increase the score of the Team by 2**/
     public void Bincrement2(View view){
         scoreOfB+=2;
         displayforTeamB(scoreOfB);
     }
 
+    /** To increase the score of the Team by 3**/
     public void Bincrement3(View view){
         scoreOfB+=3;
         displayforTeamB(scoreOfB);
     }
 
+    /** To increase the score of the Team by 1**/
     public void Bfreethrow(View view){
         scoreOfB++;
+        displayforTeamB(scoreOfB);
+    }
+
+    /** Reset the values for the next match**/
+    public void Reset(View view){
+        scoreOfA=0;
+        scoreOfB=0;
+        displayforTeamA(scoreOfA);
         displayforTeamB(scoreOfB);
     }
 
